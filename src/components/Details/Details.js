bb21ea6ca5a64c89cd3db7e958b13ca9192423e3
hscottharrison <h.scott.harrison@tcu.edu>
2017-07-20 20:48:18 -0500

    completed assignment

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import { Link } from 'react-router-dom'
 import "./Details.css"
 
 import { addToCart } from "../../ducks/product";
 
 export function Details( { addToCart, history, product } ) {
+	console.log(product)
 	const {
 		  description
 		, id
@@ -13,29 +14,35 @@ export function Details( { addToCart, history, product } ) {
 		, name
 		, price
 	} = product;
-
+	function addToCartAndRedirect(id){
+		addToCart(id)
+		history.goBack()
+	}
 	return (
 		<div className="details">
-			<h3 className="details__back-to-shop">Back to shop</h3>
+			<Link to='/shop'><h3 className="details__back-to-shop">Back to shop</h3></Link>
 			<img
-				alt={ "" /* products name */ }
+				alt={ product.name }
 				className="details__logo"
-				src={ "" /* products logo */ }
+				src={ product.logo }
 			/>
-		<h1 className="details__name">{ /* products name*/ }</h1>
-			<p className="details__description">{ /* products description*/ }</p>
+		<h1 className="details__name">{ product.name }</h1>
+			<p className="details__description">{ product.description }</p>
 			<button
 				className="details__buy"
-				onClick={ addToCart( id ) }
+				onClick={ addToCartAndRedirect }
 			>
-				Buy now for ${ /* products price */ }!
+				Buy now for ${ product.price }!
 			</button>
 		</div>
 	);
 }
 
 function mapStateToProps( state, ownProps ) {
-	return state
+	let name = ownProps.match.params.name
+	return{ product: state.products.find(elem=>{
+		return elem.name == name;
+	})}
 }
 
 export default connect( mapStateToProps, { addToCart } )( Details );
